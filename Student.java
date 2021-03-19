d4227674b37737541d37a83dfa6a5259e3bf9067
DarleneVallejos <darlenegayle07@gmail.com>
2021-03-19 15:33:55 +0800

    method 1,2 and 3

--- Student.java
+++ Student.java
@@ -1,6 +1,41 @@
 package studentinformation;
 
 public class Student 
-{ 
+{
+    private String Fname;
+    private String gen;
+    private String address;
 
+    Method 1:DarleneVallejos
+    public void getFname(String Fname)
+    {
+        this.Fname = Fname;
+    }
+ 
+    public String showFname()
+    {
+        return Fname;
+    }
+    
+    //Method 2: DarleneVallejos
+    public void getStudGender(String gen)
+    {
+        this.gen = gen;
+    }
+    
+    public String showStudGender()
+    {
+        return gen;
+    }
+    
+    //Method 3:DarleneVallejos
+     public void getAdress(String address)
+    {
+        this.address = address;
+    }
+    
+    public String showAddress()
+    {
+        return address;
+    }
 }
